d9e5aa58f4163e04a18ea9d3860c0b477b5860df
Suvrat Mishra <suvrat29@gmail.com>
2021-08-19 05:42:58 +0530

    Fixed UI related issues for mobile.
    1) Added a new dropdown menu for mobile to perform actions on categories/subcategories.
    2) Changed grid column number for 1440px+ breakpoints.
    3) Fixed tab spacing issue on mobile displays due to large tab text.

--- src/app/setup/app.setup.component.ts
+++ src/app/setup/app.setup.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, HostListener } from "@angular/core";
 import { Subject } from "rxjs";
 
 @Component({
@@ -8,8 +8,12 @@ import { Subject } from "rxjs";
 })
 
 export class AppSetupComponent {
+  @HostListener("document:click", ["$event"]) onDocumentClick(event: any) {
+    this.actionMenu = false;
+  }
   selectedTab: number = 1;
   editCategory: boolean = false;
+  actionMenu: boolean = false;
   addNewCategory: Subject<boolean> = new Subject();
 
   insertNewCategory() {
